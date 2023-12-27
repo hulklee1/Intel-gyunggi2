fdd2218b366b7d7504feb1c395c64c2d6d6c0a0b
hulklee1 <hulklee1@gmail.com>
2023-12-27 18:15:22 +0900

    1

---
+++ WinTest05-Paint/frmPaint.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WinTest05_Paint
+{
+    partial class frmPaint
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPaint));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sbLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sbLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움말ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.mnuErase = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDrawLine = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDrawCircle = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFreeDraw = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sbLabel1,
+            this.sbLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 302);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(524, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sbLabel1
+            // 
+            this.sbLabel1.AutoSize = false;
+            this.sbLabel1.Image = ((System.Drawing.Image)(resources.GetObject("sbLabel1.Image")));
+            this.sbLabel1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.sbLabel1.Name = "sbLabel1";
+            this.sbLabel1.Size = new System.Drawing.Size(150, 17);
+            // 
+            // sbLabel2
+            // 
+            this.sbLabel2.AutoSize = false;
+            this.sbLabel2.Image = ((System.Drawing.Image)(resources.GetObject("sbLabel2.Image")));
+            this.sbLabel2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.sbLabel2.Name = "sbLabel2";
+            this.sbLabel2.Size = new System.Drawing.Size(100, 17);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.보기ToolStripMenuItem,
+            this.도움말ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(524, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuOpen,
+            this.mnuSave,
+            this.toolStripMenuItem1,
+            this.mnuExit});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.Size = new System.Drawing.Size(103, 22);
+            this.mnuNew.Text = "New";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(103, 22);
+            this.mnuOpen.Text = "Open";
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(103, 22);
+            this.mnuSave.Text = "Save";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(103, 22);
+            this.mnuExit.Text = "Exit";
+            // 
+            // 보기ToolStripMenuItem
+            // 
+            this.보기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuErase,
+            this.mnuDrawLine,
+            this.mnuDrawCircle,
+            this.mnuFreeDraw});
+            this.보기ToolStripMenuItem.Name = "보기ToolStripMenuItem";
+            this.보기ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.보기ToolStripMenuItem.Text = "동작";
+            // 
+            // 도움말ToolStripMenuItem
+            // 
+            this.도움말ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAbout});
+            this.도움말ToolStripMenuItem.Name = "도움말ToolStripMenuItem";
+            this.도움말ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.도움말ToolStripMenuItem.Text = "도움말";
+            // 
+            // mnuAbout
+            // 
+            this.mnuAbout.Name = "mnuAbout";
+            this.mnuAbout.Size = new System.Drawing.Size(156, 22);
+            this.mnuAbout.Text = "About 그림판...";
+            // 
+            // Canvas
+            // 
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 24);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(524, 278);
+            this.Canvas.TabIndex = 2;
+            this.Canvas.TabStop = false;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
+            this.Canvas.Resize += new System.EventHandler(this.Canvas_Resize);
+            // 
+            // mnuErase
+            // 
+            this.mnuErase.Name = "mnuErase";
+            this.mnuErase.Size = new System.Drawing.Size(180, 22);
+            this.mnuErase.Text = "Erase";
+            this.mnuErase.Click += new System.EventHandler(this.mnuErase_Click);
+            // 
+            // mnuDrawLine
+            // 
+            this.mnuDrawLine.Name = "mnuDrawLine";
+            this.mnuDrawLine.Size = new System.Drawing.Size(180, 22);
+            this.mnuDrawLine.Text = "Line";
+            this.mnuDrawLine.Click += new System.EventHandler(this.mnuDrawLine_Click);
+            // 
+            // mnuDrawCircle
+            // 
+            this.mnuDrawCircle.Name = "mnuDrawCircle";
+            this.mnuDrawCircle.Size = new System.Drawing.Size(180, 22);
+            this.mnuDrawCircle.Text = "Circle";
+            this.mnuDrawCircle.Click += new System.EventHandler(this.mnuDrawCircle_Click);
+            // 
+            // mnuFreeDraw
+            // 
+            this.mnuFreeDraw.Name = "mnuFreeDraw";
+            this.mnuFreeDraw.Size = new System.Drawing.Size(180, 22);
+            this.mnuFreeDraw.Text = "Free draw";
+            this.mnuFreeDraw.Click += new System.EventHandler(this.mnuFreeDraw_Click);
+            // 
+            // frmPaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 324);
+            this.Controls.Add(this.Canvas);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPaint";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "내 그림판 ver 1.03";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 보기ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.ToolStripMenuItem mnuNew;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.ToolStripMenuItem 도움말ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuAbout;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel sbLabel2;
+        private System.Windows.Forms.ToolStripMenuItem mnuErase;
+        private System.Windows.Forms.ToolStripMenuItem mnuDrawLine;
+        private System.Windows.Forms.ToolStripMenuItem mnuDrawCircle;
+        private System.Windows.Forms.ToolStripMenuItem mnuFreeDraw;
+    }
+}
+
---
+++ WinTest05-Paint/frmPaint.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTest05_Paint
+{
+    public partial class frmPaint : Form
+    {
+        static float PenWidth = 1.0f;
+        Graphics g = null;
+        Pen pen = new Pen(Color.Red, PenWidth);
+        Brush brush = null;
+
+        public frmPaint()
+        {
+            InitializeComponent();
+            Canvas_Resize(null, null);
+            g = Canvas.CreateGraphics();
+            //sbLabel2.Text = $"{Canvas.Size.Width} x {Canvas.Size.Height}";
+        }
+        private void Canvas_Resize(object sender, EventArgs e)
+        {
+            sbLabel2.Text = $"{Canvas.Size.Width} x {Canvas.Size.Height}";
+            if (g != null)
+            //{
+            //    RectangleF clipRect = new RectangleF(0.0F, 0.0F, 400.0F, 400.0F);
+            //    g.SetClip(clipRect, CombineMode.Replace);
+            //}
+            g.Clip = new Region(new Rectangle(0, 0, Canvas.Size.Width, Canvas.Size.Height));
+        }
+
+        Point p1, p2, p3;
+        Point cp1, cp2, cp3;
+        int DrawMethod = 0; // 0:free,  1:Line,  2:Circle
+        bool DrawMode = false;
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            p1 = new Point(e.X, e.Y); p3 = p1; 
+            cp1 = ((Control)sender).PointToScreen(p1); cp3 = cp1;            
+            DrawMode = true;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            sbLabel1.Text = $"{e.X},{e.Y}     ";  // sdjhfasdfhkasjdfhk
+            if(DrawMode)
+            {
+                p2 = new Point(e.X, e.Y);
+                switch(DrawMethod)
+                {
+                case 0: // free draw
+                    g.DrawLine(pen, p1, p2);
+                    p1 = p2;
+                    break;
+                case 1: // Line draw
+                    Point cp2 = ((Control)sender).PointToScreen(p2);
+                    ControlPaint.DrawReversibleLine(cp1, cp3, DefaultBackColor);
+                    ControlPaint.DrawReversibleLine(cp1, cp2, DefaultBackColor);
+                    cp3 = cp2;
+                    break;
+                default: break;
+                }
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(DrawMethod == 1)
+            {
+                g.DrawLine(pen, p1, new Point(e.X, e.Y));
+            }
+            DrawMode = false;
+        }
+
+        private void mnuFreeDraw_Click(object sender, EventArgs e)
+        {
+            DrawMethod = 0;
+        }
+
+        private void mnuDrawLine_Click(object sender, EventArgs e)
+        {
+            DrawMethod = 1;
+        }
+
+        private void mnuDrawCircle_Click(object sender, EventArgs e)
+        {
+            DrawMethod = 2;
+        }
+
+        private void mnuErase_Click(object sender, EventArgs e)
+        {
+            g.Clear(DefaultBackColor);
+        }
+    }
+}
